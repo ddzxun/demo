d5f69c1ee7fd20d204e1e24264340060f421c9a3
ddzxunshu <ddzxunshu@tencent.com>
2025-09-19 15:16:09 +0800

    微笑

---
+++ c++/demo/main.c
@@ -0,0 +1,19 @@
+// main.cpp
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+
+int testFunc() {
+	// 随便写点逻辑验证
+	return 0;
+}
+
+int main() {
+    std::cout << "程序启动..." << std::endl;
+
+	// TODO: 在这里随时添加你的实验代码
+	testFunc();
+    return 0;
+}
---
+++ go/demo/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("程序启动...") // 起点
+
+	// TODO: 在这里随时添加你的实验代码
+	testFunc()
+}
+
+func testFunc() {
+	// 随便写点逻辑验证
+
+}
